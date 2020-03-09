89dee36d1418540f8ef8aed1c8c7b0bcc481097e
rutvik11062000 <rutvik.chandla@gmail.com>
2020-03-09 20:28:08 +0530

    second commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterlearnbasic/main.dart';
 
 void main() {
+  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
